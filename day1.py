d9a27b949a60f77692bc57e82f7499368c005b83
Gaurav Pandey <gauravpandey7656@gmail.com>
2022-12-01 21:34:34 +0530

    day1.py

---
+++ day1.py
@@ -0,0 +1,4 @@
+#  what is python ?
+# It is a simple and easy to understand language , which feels like reading English only .
+# this pseudo code nature of python makes it easy to understand .
+# it was developed by Guido Van Rossum .
